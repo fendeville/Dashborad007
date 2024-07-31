b3e5124b1a34bd4195f09b0aa8fc4797625d89f5
fendeville <nevilleelonge@gmail.com>
2024-07-31 11:19:12 +0100

    InfoIcon

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  UserIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -18,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Info', href: '/dashboard/Info', icon: UserIcon }
 ];
 
 export default function NavLinks() {
